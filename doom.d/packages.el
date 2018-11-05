df1bfd26976f1c0fac3b8ca8278092ab6e7087bb
Jelle Helsen <jelle.helsen@hcode.be>
2018-11-05 08:36:28 +0100

    adding doom emacs

---
+++ doom.d/packages.el
@@ -0,0 +1,15 @@
+;; -*- no-byte-compile: t; -*-
+;;; ~/.doom.d/packages.el
+
+;;; Examples:
+;; (package! some-package)
+;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
+;; (package! builtin-package :disable t)
+
+(package! projectile-rails)
+(package! rbenv)
+(package! nvm)
+(package! confluence)
+;; (package! ox-confluence)
+(package! org-jira)
+(package! emmet-mode)

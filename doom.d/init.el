678f30144ba339137fec2fe942a2266ea185f148
Jelle Helsen <jelle.helsen@devoteam.com>
2019-09-30 16:39:51 +0200

    updating i3 and emacs config

--- doom.d/init.el
+++ doom.d/init.el
@@ -182,3 +182,18 @@
        ;; reference for your own modules.
        (default +bindings +snippets +evil-commands))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("2d1fe7c9007a5b76cea4395b0fc664d0c1cfd34bb4f1860300347cdad67fb2f9" "428754d8f3ed6449c1078ed5b4335f4949dc2ad54ed9de43c56ea9b803375c23" "66d53738cc824d0bc5b703276975581b8de2b903d6ce366cd62207b5dd6d3d13" default)))
+ '(line-number-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

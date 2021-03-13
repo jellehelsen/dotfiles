4f7462bce028c60c9f3fb750fec838443397e40a
Jelle Helsen <jelle.helsen@hcode.be>
2021-03-13 16:42:18 +0100

    removing old emacs and doom config

--- emacs.d/modules/app/rss/packages.el
+++
@@ -1,5 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; app/rss/packages.el
-
-(package! elfeed :pin "362bbe5b38353d033c5299f621fea39e2c75a5e0")
-(package! elfeed-org :pin "77b6bbf222487809813de260447d31c4c59902c9")
--- emacs.d/modules/term/vterm/packages.el
+++
@@ -1,6 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; term/vterm/packages.el
-
-(package! vterm
-  :built-in 'prefer
-  :pin "fb12d7f49e2da121d9b17ed583d269166e0da087")

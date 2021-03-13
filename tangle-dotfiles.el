9c52ec640c1f373f7bb49c3b6a22f1024339c6e6
Jelle Helsen <jelle.helsen@hcode.be>
2021-03-13 16:38:23 +0100

    switching to emacs from scratch for speed

---
+++ tangle-dotfiles.el
@@ -0,0 +1,17 @@
+(require 'org)
+
+(make-directory "~/.emacs.d/" t)
+;; Don't ask when evaluating code blocks
+(setq org-confirm-babel-evaluate nil)
+
+(let* ((dotfiles-path (expand-file-name "~/.dotfiles"))
+	 (org-files (directory-files dotfiles-path nil "\\.org$")))
+
+  (defun dw/tangle-org-file (org-file)
+    (message "\n\033[1;32mUpdating %s\033[0m\n" org-file)
+    (org-babel-tangle-file (expand-file-name org-file dotfiles-path)))
+
+  ;; Tangle Systems.org first
+  (dolist (org-file org-files)
+    (unless (member org-file '("README.org" "Systems.org"))
+    	(dw/tangle-org-file org-file))))
